0c121e3458c8bde9ebb120caf1312794136c7ca4
Will Dietz <w@wdtz.org>
2018-03-27 19:00:53 -0500

    editline: wip

--- src/nix/local.mk
+++ src/nix/local.mk
@@ -2,10 +2,10 @@ programs += nix
 
 nix_DIR := $(d)
 
-nix_SOURCES := $(wildcard $(d)/*.cc) $(wildcard src/linenoise/*.cpp)
+nix_SOURCES := $(wildcard $(d)/*.cc)
 
 nix_LIBS = libexpr libmain libstore libutil libformat
 
-nix_LDFLAGS = -pthread
+nix_LDFLAGS = $(EDITLINE_LIBS) -pthread
 
 $(eval $(call install-symlink, nix, $(bindir)/nix-hash))
