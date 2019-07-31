2b95f39a12136f86c23bcd0b21352ec7404a84fa
Will Dietz <w@wdtz.org>
2019-07-31 17:29:10 -0500

    Revert printing sri hashes also, nixpkgs and auto-updaters not ready
    This reverts commit bd8cda614a168d4ac6fa02dba4a5a051be05df56.
    This reverts commit 3e76284901c57e0b0897f52de2b3ffeb24225529.

--- src/libstore/local-store.cc
+++ src/libstore/local-store.cc
@@ -1021,11 +1021,11 @@ void LocalStore::addToStore(const ValidPathInfo & info, Source & source,
             auto hashResult = hashSink.finish();
 
             if (hashResult.first != info.narHash)
-                throw Error("hash mismatch importing path '%s';\n  wanted: %s\n  got:    %s\n  sri:    %s",
-                    info.path, info.narHash.to_string(), hashResult.first.to_string(), hashResult.first.to_string(SRI));
+                throw Error("hash mismatch importing path '%s';\n  wanted: %s\n  got:    %s",
+                    info.path, info.narHash.to_string(), hashResult.first.to_string());
 
             if (hashResult.second != info.narSize)
-                throw Error("size mismatch importing path '%s';\n  wanted: %s\n  got:   %s\n",
+                throw Error("size mismatch importing path '%s';\n  wanted: %s\n  got:   %s",
                     info.path, info.narSize, hashResult.second);
 
             autoGC();
